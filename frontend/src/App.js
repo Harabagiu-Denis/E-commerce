a84b7ab2a2cf6c0ba141d3234ee94f34b1c61dd0
Harabagiu-Denis <harabagiudenis98@gmail.com>
2022-04-06 13:12:46 +0300

    React setup 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-      <h1>Bine ati venit</h1>
+    <Header />
+    <main className="py-3">
+      <Container> <h1>Bine ati venit</h1></Container>
+   
+    </main>
+     <Footer />
     </>
   );
 }
