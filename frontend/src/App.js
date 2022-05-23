ded62b69cf9b4c831e55d7eb61548f5e7515e6bc
Harabagiu-Denis <harabagiudenis98@gmail.com>
2022-05-23 11:44:23 +0300

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
     <>
     <Header />
     <main className="py-3">
-      <Container> <h1>Bine ati venit</h1></Container>
+      <Container> 
+        <HomeScreen />
+      </Container>
    
     </main>
      <Footer />
